46affc4932b413f2e2b13996982e150824abfa34
jagery07 <78416671+jagery07@users.noreply.github.com>
2021-04-03 12:26:46 +0530

    Update n_fractions.c

--- n_fractions.c
+++ n_fractions.c
@@ -1,49 +1,65 @@
 //WAP to find the sum of n fractions.
 #include<stdio.h>
-struct variables
-{
-    int a1;
-    int b1;
-    int a2;
-    int b2;
-};
+#include<stdlib.h>
 struct fraction
 {
-    int n;
-    int d;
+    int num;
+    int den;
 };
+struct fraction input_value();
+struct fraction add_frac(struct fraction f1, struct fraction f2);
+int greatest_common_denominator(int x, int y);
+void print_values(struct fraction f1,struct fraction f2,struct fraction ans);
 
-int sum_fraction(struct variables);
-int main(void)
+int main()
 {
-    struct variables var;
-    struct fraction frac;
-    printf("Enter 1st numerator: ");
-    scanf("%d",&var.a1);
-    printf("\nEnter 1st denominator: ");
-    scanf("%d",&var.b1);
-    printf("\nEnter 2nd numerator: ");
-    scanf("%d",&var.a2);
-    printf("\nEnter 2nd denominator: ");
-    scanf("%d",&var.b2);
-
-    sum_fraction(var);
-    return 0;
+    struct fraction frac1, frac2, frac3;
+    frac1=input_value();
+    frac2=input_value();
+    frac3=add_frac(frac1,frac2);
+    print_values(frac1,frac2,frac3);
 }
 
-int sum_fraction(struct variables var)
+struct fraction input_value()
 {
-    struct fraction fr1;
-    if(var.b1==var.b2)
+    struct fraction f1;
+    printf("Enter numerator:\n");
+    scanf("%d",&f1.num);
+    printf("Enter denominator:\n");
+    scanf("%d",&f1.den);
+    return f1;
+}
+
+int greatest_common_denominator(int x, int y)
+{
+    int i,var;
+    var=1;
+    for (i=2;i<=x && i<=y;i++)
+    {
+        if ((x%i==0) && (y%i==0))
         {
-            fr1.n=var.a1+var.a2;
-            fr1.d=var.b1;
-            printf("\nSum of 2 fractions is %d/%d",fr1.n,fr1.d);
+            var=i;
         }
-    else
-        {
-            fr1.n=var.a1*var.b2+var.a2*var.b1;
-            fr1.d=var.b1*var.b2;
-            printf("\nSum of 2 fractions is %d/%d",fr1.n,fr1.d);
-        }
+    }
+    return var;
+}
+
+struct fraction add_frac(struct fraction f1, struct fraction f2)
+{
+    int gcd;
+    struct fraction answer;
+    answer.num=(f1.num*f2.den)+(f2.num*f1.den);
+    answer.den=f1.den*f2.den;
+
+    gcd=greatest_common_denominator(answer.num,answer.den);
+
+    answer.num=answer.num/gcd;
+    answer.den=answer.den/gcd;
+
+    return answer;
+}
+
+void print_values(struct fraction f1,struct fraction f2,struct fraction answer)
+{
+    printf("Sum of %d/%d & %d/%d is %d/%d",f1.num,f1.den,f2.num,f2.den,answer.num,answer.den);
 }
